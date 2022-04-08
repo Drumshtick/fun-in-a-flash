de439e3b8ad972208a510892259e96d3a098c431
Drumshtick <ekodnar@outlook.com>
2022-04-08 13:46:06 -0600

    moved GameHeader to seperate component

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import { connect } from 'react-redux';
 import styles from '../../styles/Play.module.scss';
-import CloseIcon from '@mui/icons-material/Close';
-import StarsIcon from '@mui/icons-material/Stars';
+import { GameHeader } from './index';
 
 function mapStateToProps(state) {
   return {
@@ -14,20 +14,7 @@ function mapStateToProps(state) {
 const Play = () => {
   return (
     <div className={styles.container}>
-      <header>
-        <CloseIcon />
-        <div className={styles.scoreContainer}>
-          <p>SCORE</p>
-          <div className={styles.scoreValue}>
-            <p>1074</p>
-            <StarsIcon />
-          </div>
-        </div>
-        <div className={styles.questionCountContainer}>
-          <p>QUESTION</p>
-          <p className={styles.questionCount}>5/6</p>
-        </div>
-      </header>
+      <GameHeader />
     </div>  
   );
 };
