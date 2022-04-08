89a51f80329e3b9f103e792e0238df80ff13d211
Drumshtick <ekodnar@outlook.com>
2022-04-07 19:25:23 -0600

    imported Play

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,18 @@ import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import styles from '../styles/Home.module.scss';
 // Dynamic imports allows the loading of modules on demand, prevents loading of unused code 
-const GameInfo = dynamic(() => import('../components/hello'));
-
+const GameInfo = dynamic(() => import('../components/GameInfo/GameInfo'));
+const Play = dynamic(() => import('../components/Play/Play'));
 export default function Home() {
   return (
-    <div>
+    <div className={styles.gameContainer}>
       <Head>
         <title>Fun in a flash</title>
         <meta name="description" content="A math flash card app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* <GameInfo /> */}
+      <Play />
     </div>
   )
 }
