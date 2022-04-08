1275196966f6e18f026fa62615cf07b826960731
Drumshtick <ekodnar@outlook.com>
2022-04-07 19:24:52 -0600

    basic layout for gameInfo

--- src/components/GameInfo/GameInfo.js
+++ src/components/GameInfo/GameInfo.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import StarsIcon from '@mui/icons-material/Stars';
+import CasinoIcon from '@mui/icons-material/Casino';
+import { Button } from '@mui/material';
+import styles from '../../styles/GameInfo.module.scss';
 
 function mapStateToProps(state) {
   return {
@@ -9,10 +13,30 @@ function mapStateToProps(state) {
 }
 
 
-const StartGame = () => {
+const GameInfo = () => {
   return (
-    <div></div>
+    <div className={styles.mainContainer}>
+      <div className={styles.header}>
+        <p>Fun in a flash:</p>
+        <h1>Math Cards!</h1>
+      </div>
+      <div className={styles.highScoreContainer}>
+        <p className={styles.highScoreLabel}>CURRENT HIGH SCORE</p>
+        <div className={styles.highScoreValue}>
+          <h2>1208</h2>
+          <StarsIcon />
+        </div>
+      </div>
+      <div className={styles.playButtonContainer}>
+        <Button
+          className={styles.playButton}
+        >
+          start game
+          <CasinoIcon />
+        </Button>
+      </div>
+    </div>
   );
 };
 
-export default connect(mapStateToProps)(StartGame);
+export default connect(mapStateToProps)(GameInfo);
