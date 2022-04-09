56e48dbd8ffbc06ed1c43ccfe68d9c8e4dd3a175
Drumshtick <ekodnar@outlook.com>
2022-04-09 15:17:26 -0600

    added import

--- src/components/Play/index.js
+++ src/components/Play/index.js
@@ -2,3 +2,4 @@ export { default as GameHeader } from './GameHeader';
 export { default as QuestionArea } from './QuestionArea';
 export { default as OnscreenInput } from './OnscreenInput';
 export { default as KeyboardInput } from './KeyboardInput';
+export { default as QuestionScore } from './QuestionScore';
