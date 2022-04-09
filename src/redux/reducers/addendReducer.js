fb7c9e5bd2031c987c69f0d8cbd276b3eff2cc42
Drumshtick <ekodnar@outlook.com>
2022-04-09 17:05:09 -0600

    remove console.logs

--- src/redux/reducers/addendReducer.js
+++ src/redux/reducers/addendReducer.js
@@ -9,7 +9,6 @@ const addendReducer = (state = initState, action) => {
 
   if (action.type === 'NEW_QUESTION') {
     const { value1, value2 } = makeQuestion();
-    console.log("new state: ", "value1: ", value1, "value2: ", value2)
     return { value1, value2 };
   }
   
