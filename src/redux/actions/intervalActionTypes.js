e76e18977fd0001cd2c840ef1fda9b75f9efdcef
Drumshtick <ekodnar@outlook.com>
2022-04-09 17:05:01 -0600

    added action types

---
+++ src/redux/actions/intervalActionTypes.js
@@ -0,0 +1,2 @@
+export const SET_INTERVAL_ID = (value) => ({ type: 'SET_INTERVAL_ID', value });
+export const CLEAR_INTERVAL_ID = () => ({ type: 'CLEAR_INTERVAL_ID' });
---
+++ src/redux/actions/totalScoreActionTypes.js
@@ -0,0 +1,2 @@
+export const INCREASE_TOTAL_SCORE = (value) => ({ type: 'INCREASE_TOTAL_SCORE', value });
+export const RESET_TOTAL_SCORE = () => ({ type: 'RESET_TOTAL_SCORE' });
