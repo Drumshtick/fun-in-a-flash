4f75a1da2c21060f63ea4c3a8a20f19f1a0f360b
Drumshtick <ekodnar@outlook.com>
2022-04-09 13:26:31 -0600

    started game mechanics

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import styles from '../../styles/Play.module.scss';
-import { GameHeader, QuestionArea, GameInput } from './index';
-
+import { GameHeader, QuestionArea, OnscreenInput } from './index';
+import { NEW_QUESTION } from '../../redux/actions/addendActionTypes';
+import { RESET_GUESS } from '../../redux/actions/InputActionTypes';
+import { CORRECT_ANSWER } from '../../redux/actions/correctActionTypes';
 function mapStateToProps(state) {
   return {
-    // State needed in StartGame here
-    // count: state.count
+    value1: state.addend.value1,
+    value2: state.addend.value2,
+    answer: state.input.answer
   };
 }
 
-const Play = () => {
+const Play = ({
+  dispatch,
+  answer,
+  value1,
+  value2
+}) => {
+
+  useEffect(() => {
+    dispatch(NEW_QUESTION());
+  }, [ dispatch ]);
+
+  useEffect(() => {
+    if (parseInt(answer) === parseInt(value1) + parseInt(value2)) {
+      console.log("CORRECT!");
+      dispatch(CORRECT_ANSWER());
+      dispatch(NEW_QUESTION());
+      dispatch(RESET_GUESS());
+      return;
+    }
+    console.log("INCORRECT");
+  }, [ answer, value1, value2, dispatch ]);
+
   return (
     <div className={styles.container}>
       <GameHeader />
       <QuestionArea />
-      <GameInput />
+      <OnscreenInput />
     </div>  
   );
 };
