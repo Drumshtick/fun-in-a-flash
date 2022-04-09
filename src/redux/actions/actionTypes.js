d6efe6e360e4ee284b822ec30fdf04ffba510ab6
Drumshtick <ekodnar@outlook.com>
2022-04-08 19:19:47 -0600

    created action types

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,3 @@
+export const INPUT_NUMBER = (value) => ({ type: 'INPUT_NUMBER', value });
+export const DELETE_NUMBER = () => ({ type: 'DELETE_NUMBER' });
+export const RESET_GUESS = () => ({ type: 'RESET_GUESS'});
