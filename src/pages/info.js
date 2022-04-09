c9344bf89cc4d3e50e40d2da450862fc06b38d5d
Drumshtick <ekodnar@outlook.com>
2022-04-09 13:28:00 -0600

    test route for info page

--- src/pages/info.js
+++ src/pages/info.js
@@ -2,12 +2,21 @@ import React from 'react';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import styles from '../styles/Home.module.scss';
+import { connect } from 'react-redux';
 const GameInfo = dynamic(() => import('../components/GameInfo/GameInfo'));
 
-export default function Info() {
+function mapStateToProps(state) {
+  return {
+    answer: state.input.answer
+  }
+}
+
+const Info = (props) => {
   return (
     <div className={styles.gameContainer}>
       <GameInfo />
     </div>
   );
 };
+
+export default connect(mapStateToProps)(Info);
