736722dade6f6a0a53b776ebcd74e5eae7f2628a
Drumshtick <ekodnar@outlook.com>
2022-04-09 17:05:25 -0600

    added imports

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,3 +3,5 @@ export { default as addendReducer } from './addendReducer';
 export { default as correctReducer } from './correctReducer';
 export { default as scoreReducer } from './scoreReducer';
 export { default as questionReducer } from './questionReducer';
+export { default as totalScoreReducer } from './totalScoreReducer';
+export { default as intervalReducer } from './intervalReducer';
