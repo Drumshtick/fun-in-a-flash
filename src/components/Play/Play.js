6bdc29f73e7d0938e2b28bf25f525eefeb433f1c
Drumshtick <ekodnar@outlook.com>
2022-04-09 15:17:52 -0600

    working on game mechanics

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -1,15 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { connect } from 'react-redux';
 import styles from '../../styles/Play.module.scss';
 import { GameHeader, QuestionArea, OnscreenInput } from './index';
 import { NEW_QUESTION } from '../../redux/actions/addendActionTypes';
 import { RESET_GUESS } from '../../redux/actions/InputActionTypes';
 import { CORRECT_ANSWER } from '../../redux/actions/correctActionTypes';
+import { DECREASE_SCORE, RESET_SCORE } from '../../redux/actions/scoreActionTypes';
+import sleep from '../../helpers/sleep';
+import clearSleepIds from '../../helpers/clearSleepIds';
+const CONSTANTS = {
+  'INITIAL_SCORE': parseInt(process.env.NEXT_PUBLIC_INITIAL_SCORE),
+  'REDUCE_SCORE_BY': parseInt(process.env.NEXT_PUBLIC_REDUCE_SCORE_BY),
+  'REDUCE_INTERVAL': parseInt(process.env.NEXT_PUBLIC_REDUCE_INTERVAL),
+  'TOTAL_INTERVALS': () => {
+    return this['INITIAL_SCORE'] / this['REDUCE_SCORE_BY']
+  }
+}
+
 function mapStateToProps(state) {
   return {
     value1: state.addend.value1,
     value2: state.addend.value2,
-    answer: state.input.answer
+    answer: state.input.answer,
+    score: state.score.score
   };
 }
 
@@ -20,20 +33,57 @@ const Play = ({
   value2
 }) => {
 
-  useEffect(() => {
-    dispatch(NEW_QUESTION());
-  }, [ dispatch ]);
+  const scoreDropper = useCallback(() => {
+    const { TOTAL_INTERVALS, REDUCE_INTERVAL  } = CONSTANTS
+    const sleepIds = [];
+    for (let i = 0; i < TOTAL_INTERVALS; i++) {
+      const sleepId = sleep(REDUCE_INTERVAL * (i + 1) )
+      .then(() => {
+        dispatch(DECREASE_SCORE());
+      });
+      sleepIds.push(sleepId);
+    }
+    return sleepIds
+  }, [ dispatch ])
+
+
+
+
 
   useEffect(() => {
-    if (parseInt(answer) === parseInt(value1) + parseInt(value2)) {
-      console.log("CORRECT!");
+    const startQuestions = () => {
+      console.log("HERE");
+      dispatch(RESET_SCORE());
+      dispatch(NEW_QUESTION());
+      const sleepIds = scoreDropper();
+      return sleepIds;
+    };
+
+    var sleepIds = startQuestions()
+    return () => {
+      clearSleepIds(sleepIds);
+    }
+  }, [ dispatch, scoreDropper ]);
+
+  useEffect(() => {
+
+    const newQuestion = () => {
       dispatch(CORRECT_ANSWER());
       dispatch(NEW_QUESTION());
       dispatch(RESET_GUESS());
+      return scoreDropper();
+    };
+
+    if (parseInt(answer) === parseInt(value1) + parseInt(value2)) {
+      console.log('CORRECT!');
+      var sleepIds = newQuestion();
       return;
     }
-    console.log("INCORRECT");
-  }, [ answer, value1, value2, dispatch ]);
+    console.log('INCORRECT');
+    return () => {
+      clearSleepIds(sleepIds);
+    }
+  }, [ answer, value1, value2, dispatch, scoreDropper ]);
 
   return (
     <div className={styles.container}>
