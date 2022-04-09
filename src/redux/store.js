2cf187bd1c77b6254c064f30a31d969c9ef6004a
Drumshtick <ekodnar@outlook.com>
2022-04-09 13:28:11 -0600

    added more reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,13 @@
 import { createStore, combineReducers } from 'redux';
-// import reducers here
-import keyboardReducer from './reducers/keyboard';
+import {
+  inputReducer,
+  addendReducer,
+  correctReducer
+} from './reducers/index';
 const rootReducer = combineReducers({
-    // Declare individual reducers here
-    // profile: profileReducer,
-    keyboard: keyboardReducer
+    input: inputReducer,
+    addend: addendReducer,
+    answerCorrect: correctReducer
 });
-export const store = createStore(
-    rootReducer,
-    typeof window !== "undefined" &&
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+
+export const store = createStore(rootReducer);
