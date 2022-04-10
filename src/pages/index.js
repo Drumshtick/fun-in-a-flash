53306f63981c45a9d80f6cea1f43792318b99697
Drumshtick <ekodnar@outlook.com>
2022-04-09 19:04:34 -0600

    implemented views

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import styles from '../styles/Home.module.scss';
 // Dynamic imports allows the loading of modules on demand, prevents loading of unused code 
 const GameInfo = dynamic(() => import('../components/GameInfo/GameInfo'));
 const Play = dynamic(() => import('../components/Play/Play'));
-export default function Home() {
+
+const mapStateToProps = (state) => {
+  return {
+    view: state.view.view
+  }
+};
+
+const Home = ({ view }) => {
   return (
     <div className={styles.gameContainer}>
       <Head>
@@ -13,8 +21,14 @@ export default function Home() {
         <meta name="description" content="A math flash card app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <GameInfo /> */}
-      <Play />
+      {view !== 'play' ? (
+        <GameInfo />
+      ) : (
+        <Play />
+      )}
+      
     </div>
   )
-}
+};
+
+export default connect(mapStateToProps)(Home);
