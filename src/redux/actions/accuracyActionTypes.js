d627249d4226e35a5e04d764f5aadaacb6b45c3c
Drumshtick <ekodnar@outlook.com>
2022-04-09 19:03:43 -0600

    added new action types

---
+++ src/redux/actions/accuracyActionTypes.js
@@ -0,0 +1,2 @@
+export const INCREASE_ACCURACY = () => ({ type: 'INCREASE_ACCURACY' });
+export const RESET_ACCURACY = () => ({ type: 'RESET_ACCURACY'});
--- src/redux/actions/correctActionTypes.js
+++ src/redux/actions/correctActionTypes.js
@@ -1,1 +1,2 @@
 export const CORRECT_ANSWER = () => ({ type: 'CORRECT_ANSWER' });
+export const INCORRECT_ANSWER = () => ({ type: 'INCORRECT_ANSWER' });
