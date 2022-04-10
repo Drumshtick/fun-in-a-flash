5058a06867eb514d93592b40f670ab87bc15695b
Drumshtick <ekodnar@outlook.com>
2022-04-10 13:55:09 -0600

    del boilerplate

--- src/pages/api/hello.js
+++
@@ -1,5 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// Return promise to avoid Next error 'api did not send response'
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
