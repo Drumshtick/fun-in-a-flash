37750a2bc271e9479d67aba8bcbf1ec097f14e68
erikkodnar <erik@gskinner.com>
2022-05-03 15:15:50 -0600

    removed interval clearing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { CLEAR_INTERVAL_ID } from '../redux/actions/scoreIntervalActionTypes';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import styles from '../styles/Home.module.scss';
+import useFullScreenAPI from '../hooks/useFullScreenAPI';
 const GameInfo = dynamic(() => import('../components/GameInfo/GameInfo'));
 const Play = dynamic(() => import('../components/Play/Play'));
 const FireworksLottie = dynamic(() => import('../components/FireworksLottie'));
+
 const mapStateToProps = (state) => {
   return {
     view: state.view.view,
-    scoreInterval: state.scoreInterval.ID
   }
 };
 
-const Home = ({ view, scoreInterval, dispatch }) => {
-
-  useEffect(() => {
-    if (view !== 'play') {
-      clearInterval(scoreInterval);
-      dispatch(CLEAR_INTERVAL_ID());
-    }
-  })
+const Home = ({ view }) => {
 
   return (
     <div className={styles.gameContainer}>
