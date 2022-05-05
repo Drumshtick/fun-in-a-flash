e1b6e35a2dd274e7c3b5730ef81274ec1d67e669
erikkodnar <erik@gskinner.com>
2022-05-05 14:50:24 -0600

    To TS

--- src/hooks/useScoreDropper.ts
+++ src/hooks/useScoreDropper.ts
@@ -1,23 +1,24 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../redux/store';
 import { DECREASE_SCORE } from '../redux/actions/scoreActionTypes';
 import { SET_INTERVAL_ID } from '../redux/actions/scoreIntervalActionTypes';
 
-const REDUCE_INTERVAL = parseInt(process.env.NEXT_PUBLIC_REDUCE_SCORE_INTERVAL);
+const REDUCE_INTERVAL: number = parseInt(process.env.NEXT_PUBLIC_REDUCE_SCORE_INTERVAL);
 
 const useScoreDropper = () => {
-  const dispatch = useDispatch();
-  const [ time, setTime ] = useState(null);
+  const dispatch: AppDispatch = useDispatch();
+  const [ time, setTime ] = useState<null | number>(null);
   const [ dropScore, setDropScore ] = useState(false);
-  const [ questionStartTime, setQuestionStartTime ] = useState(null);
+  const [ questionStartTime, setQuestionStartTime ] = useState<null | number>(null);
 
-  const scoreDropper = useCallback(() => {
+  const scoreDropper = useCallback((): void => {
     setQuestionStartTime(time);
     setDropScore(true)
   }, [ time ])
 
   useEffect(() => {
-    const intervalID = setInterval(() => {
+    const intervalID: NodeJS.Timer = setInterval(() => {
       const secondsSinceEpoch = Math.round(Date.now() / 1000)
       setTime(secondsSinceEpoch);
     }, 1000);
