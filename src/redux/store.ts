b140b7027575d14e988c34a59f7187b453442ecb
erikkodnar <erik@gskinner.com>
2022-05-05 14:50:44 -0600

    To TS, export Types

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -22,3 +22,22 @@ export const rootReducer = combineReducers({
 });
 
 export const store = createStore(rootReducer);
+export type AppDispatch = typeof store.dispatch;
+export type State = {
+  view?: {view: string},
+  totalScore?: {totalScore: number},
+  highScore?: {highScore: number},
+  input?: {answer: string},
+  score?: {score: number},
+  scoreInterval?: {ID: number},
+  results: {correct: number, results: Array<Results>},
+  newHighScore: {madeHighScore: boolean},
+  madeHighScore: {madeHighScore: boolean},
+}
+export type  Results = {
+  value1: number,
+  value2: number,
+  answer: number,
+  score: number,
+  correct: boolean,
+}
