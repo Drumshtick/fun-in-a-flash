77e03eb8c6cae61e4cabaafcba494ce21f0a6e6e
erikkodnar <erik@gskinner.com>
2022-05-06 08:31:59 -0600

    format changes

--- src/components/Play/EnterAnswerButton.tsx
+++ src/components/Play/EnterAnswerButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import IconButton from '@mui/material/IconButton';
-import styles from '../../styles/EnterAnswer.module.scss';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import styles from '../../styles/EnterAnswer.module.scss';
 
 interface EnterAnswer {
   submitAnswer: Function,
