6bb49e761162a665b7ab7bbd18a0fe5e6b60d50a
erikkodnar <erik@gskinner.com>
2022-05-09 09:46:10 -0600

    To TS

--- src/components/Play/QuestionScore.tsx
+++ src/components/Play/QuestionScore.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+import {State} from '../../redux/store';
 import StarsIcon from '@mui/icons-material/Stars';
 import styles from '../../styles/QuestionScore.module.scss';
 
-function mapStateToProps(state) {
+function mapStateToProps(state: State) {
   return {
     score: state.score.score,
     view: state.view.view
   };
 }
 
-const QuestionScore = ({ score, reviewScore, view, changeScore }) => {
+interface QuestionScore {
+  score: number,
+  reviewScore: number,
+  view: string,
+  changeScore: boolean
+}
+
+const QuestionScore: React.FC<QuestionScore> = ({ score, reviewScore, view, changeScore }) => {
   return (
     <div className={`${styles.score} ${changeScore && styles.scoreChange} ${score <= 100 && styles.lowScore}`}>
       <p>
