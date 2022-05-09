026ab1d4d61f5734ebe76b8e0ef23de16bc91dd9
erikkodnar <erik@gskinner.com>
2022-05-09 09:46:34 -0600

    To TS

--- src/components/Play/ResultPrompt.tsx
+++ src/components/Play/ResultPrompt.tsx
@@ -3,7 +3,11 @@ import DoNotDisturbIcon from '@mui/icons-material/DoNotDisturb';
 import CheckIcon from '@mui/icons-material/Check';
 import styles from '../../styles/ResultPrompt.module.scss';
 
-const ResultPrompt = ({ correct }) => {
+interface ResultPrompt {
+  correct: boolean
+}
+
+const ResultPrompt: React.FC<ResultPrompt> = ({ correct }) => {
   return (
     <div className={styles.container}>
       {correct && <CheckIcon className={styles.correct}/>}
