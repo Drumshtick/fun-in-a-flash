22f2d1e35b9ae45acc41a61a1862d2aac779907f
erikkodnar <erik@gskinner.com>
2022-05-09 09:43:59 -0600

    To TS

--- src/components/FireworksLottie.tsx
+++ src/components/FireworksLottie.tsx
@@ -1,25 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import Lottie from 'lottie-react';
 import fireworks from '../../public/lottie/98350-fireworks.json';
-import { connect } from 'react-redux';
+import {State} from '../redux/store';
+import {connect} from 'react-redux';
 import sleep from '../helpers/sleep';
 
 import styles from '../styles/FireworksLottie.module.scss';
 
 const FIREWORKS_STOP_DELAY = parseInt(process.env.NEXT_PUBLIC_FIREWORKS_STOP_DELAY);
 
-const mapStateToProps = (state) => {
+interface FireworksLottie {
+  madeHighScore: boolean,
+  view: string
+}
+
+const mapStateToProps = (state: State) => {
   return {
     madeHighScore: state.madeHighScore.madeHighScore,
     view: state.view.view
   };
 };
 
-const FireworksLottie = ({ madeHighScore, view }) => {
+const FireworksLottie: React.FC<FireworksLottie> = ({ madeHighScore, view }) => {
   const [ play, setPlay ] = useState(false);
 
   useEffect(() => {
-    const controlLottie = async () => {
+    const controlLottie = async (): Promise<null> => {
       if (view !== 'done') return;
       setPlay(true);
       await sleep(FIREWORKS_STOP_DELAY);
@@ -29,7 +35,7 @@ const FireworksLottie = ({ madeHighScore, view }) => {
     if (madeHighScore) {
       controlLottie();
     }
-  }, [ madeHighScore, view ])
+  }, [madeHighScore, view])
 
   return (
     <Lottie
