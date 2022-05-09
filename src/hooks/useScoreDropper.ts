fcbe98d809752c1ae8f30b726809a10a4b4925c0
erikkodnar <erik@gskinner.com>
2022-05-09 00:01:02 -0600

    cleaned up

--- src/hooks/useScoreDropper.ts
+++ src/hooks/useScoreDropper.ts
@@ -3,41 +3,32 @@ import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../redux/store';
 import { DECREASE_SCORE } from '../redux/actions/scoreActionTypes';
 import { SET_INTERVAL_ID } from '../redux/actions/scoreIntervalActionTypes';
+import CONSTANTS from '../helpers/constants';
 
-const REDUCE_INTERVAL: number = parseInt(process.env.NEXT_PUBLIC_REDUCE_SCORE_INTERVAL);
+const {
+  REDUCE_INTERVAL
+} = CONSTANTS;
 
-const useScoreDropper = () => {
+const useScoreDropper = (score: number, disableSubmit: boolean) => {
   const dispatch: AppDispatch = useDispatch();
-  const [ time, setTime ] = useState<null | number>(null);
   const [ dropScore, setDropScore ] = useState(false);
-  const [ questionStartTime, setQuestionStartTime ] = useState<null | number>(null);
 
-  const scoreDropper = useCallback((): void => {
-    setQuestionStartTime(time);
+  const scoreDropper = (): void => {
     setDropScore(true)
-  }, [ time ])
-
-  useEffect(() => {
-    const intervalID: NodeJS.Timer = setInterval(() => {
-      const secondsSinceEpoch = Math.round(Date.now() / 1000)
-      setTime(secondsSinceEpoch);
-    }, 1000);
-    dispatch(SET_INTERVAL_ID(intervalID));
-    return () => {
-      clearInterval(intervalID);
-    };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  };
 
   useEffect(() => {
     if (!dropScore) return;
-    if (time - questionStartTime >= REDUCE_INTERVAL) {
-      setQuestionStartTime(time);
-      dispatch(DECREASE_SCORE());
-    }
-  }, [dropScore, time, questionStartTime, dispatch]);
+    const ID: ReturnType<typeof setTimeout> = setTimeout(() => {
+        dispatch(DECREASE_SCORE())
+      }, REDUCE_INTERVAL)
+    dispatch(SET_INTERVAL_ID(ID));
+    return () => {
+      clearTimeout(ID);
+    };
+  }, [dropScore, dispatch, score])
 
-  return {scoreDropper, setDropScore, setQuestionStartTime};
+  return {scoreDropper, setDropScore};
 };
 
 export default useScoreDropper;
